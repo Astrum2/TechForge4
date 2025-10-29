ee0cc8cefc676d8ada02d52b7ffaba68c93eb9f2
João Pedro Nascimento Agostinho <149485052+Astrum2@users.noreply.github.com>
2025-10-29 15:45:31 -0300

    Create exercicio1.ts

---
+++ Trilha 4/exercicio1.ts
@@ -0,0 +1,13 @@
+export interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
+
+export class ItemLoja implements Produto {
+  constructor(
+    public id: number,
+    public nome: string,
+    public preco: number
+  ) {}
+}
